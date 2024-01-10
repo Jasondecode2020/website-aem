b67f4a5580d36aa9b19e808ec56c04877be1b12c
Zhenjie Zhang <Zhenjie.Zhang@det.nsw.edu.au>
2024-01-10 14:45:55 +1100

    add links for microsoft

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,6 +13,21 @@ import {
   loadCSS,
 } from './aem.js';
 
+/**
+ * Wraps images followed by links within a matching <a> tag.
+ * @param {Element} container The container element
+ */
+function wrapImgsInLinks(container) {
+  const pictures = container.querySelectorAll('picture');
+  pictures.forEach((pic) => {
+    const link = pic.nextElementSibling;
+    if (link && link.tagName === 'A' && link.href) {
+      link.innerHTML = pic.outerHTML;
+      pic.replaceWith(link);
+    }
+  });
+}
+
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 
 /**
@@ -68,6 +83,7 @@ export function decorateMain(main) {
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
+  wrapImgsInLinks(main);
 }
 
 /**
